b278622222ef008c1bc982bba0ac46c81887dfff
Murdo Ergeaux <visual.melon@gmail.com>
2013-12-20 20:11:49 +0000

    Initial Commit

---
+++ PyBMP/MainForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Freddie
+ * Date: 20/12/2013
+ * Time: 16:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace PyBMP
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.inImg = new System.Windows.Forms.PictureBox();
+			this.outImg = new System.Windows.Forms.PictureBox();
+			this.codeF = new System.Windows.Forms.TextBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.inImg)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.outImg)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.codeF);
+			this.splitContainer1.Size = new System.Drawing.Size(402, 249);
+			this.splitContainer1.SplitterDistance = 103;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer2.Name = "splitContainer2";
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.Controls.Add(this.inImg);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.Controls.Add(this.outImg);
+			this.splitContainer2.Size = new System.Drawing.Size(402, 103);
+			this.splitContainer2.SplitterDistance = 195;
+			this.splitContainer2.TabIndex = 0;
+			// 
+			// inImg
+			// 
+			this.inImg.BackColor = System.Drawing.Color.White;
+			this.inImg.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.inImg.Location = new System.Drawing.Point(0, 0);
+			this.inImg.Name = "inImg";
+			this.inImg.Size = new System.Drawing.Size(195, 103);
+			this.inImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.inImg.TabIndex = 0;
+			this.inImg.TabStop = false;
+			this.inImg.Click += new System.EventHandler(this.InImgClick);
+			// 
+			// outImg
+			// 
+			this.outImg.BackColor = System.Drawing.Color.White;
+			this.outImg.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.outImg.Location = new System.Drawing.Point(0, 0);
+			this.outImg.Name = "outImg";
+			this.outImg.Size = new System.Drawing.Size(203, 103);
+			this.outImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.outImg.TabIndex = 1;
+			this.outImg.TabStop = false;
+			this.outImg.Click += new System.EventHandler(this.OutImgClick);
+			// 
+			// codeF
+			// 
+			this.codeF.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.codeF.Location = new System.Drawing.Point(0, 0);
+			this.codeF.Multiline = true;
+			this.codeF.Name = "codeF";
+			this.codeF.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.codeF.Size = new System.Drawing.Size(402, 142);
+			this.codeF.TabIndex = 0;
+			this.codeF.Text = "for y in range(target.height):\r\n\tfor x in range(target.width):\r\n\t\tcol = surfs[0]." +
+			"getCol_clamp(x, y)\r\n\t\ttarget.setCol_clamp(x, y, col)";
+			this.codeF.WordWrap = false;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.processToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(402, 24);
+			this.menuStrip1.TabIndex = 3;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// processToolStripMenuItem
+			// 
+			this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+			this.processToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+			this.processToolStripMenuItem.Text = "Process";
+			this.processToolStripMenuItem.Click += new System.EventHandler(this.ProcessToolStripMenuItemClick);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.DefaultExt = "png";
+			this.saveFileDialog1.Filter = "PNGs|*.png";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(402, 273);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.menuStrip1);
+			this.Name = "MainForm";
+			this.Text = "PyBMP";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			this.splitContainer1.ResumeLayout(false);
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			this.splitContainer2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.inImg)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.outImg)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox codeF;
+		private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.PictureBox outImg;
+		private System.Windows.Forms.PictureBox inImg;
+		private System.Windows.Forms.SplitContainer splitContainer2;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+	}
+}
---
+++ PyBMP/MainForm.cs
@@ -0,0 +1,389 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Freddie
+ * Date: 20/12/2013
+ * Time: 16:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using sdi = System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using py = IronPython;
+using sing = Microsoft.Scripting;
+
+using System.Runtime.InteropServices;
+
+namespace PyBMP
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		[StructLayout(LayoutKind.Explicit)]
+		public struct colour
+		{
+			[FieldOffset(0)]
+			public byte b;
+			[FieldOffset(1)]
+			public byte g;
+			[FieldOffset(2)]
+			public byte r;
+			[FieldOffset(3)]
+			public byte a;
+			
+			[FieldOffset(0)]
+			public int argb;
+			
+			public colour(int argbN) : this()
+			{
+				argb = argbN;
+			}
+			
+			public colour(byte aN, byte rN, byte gN, byte bN) : this()
+			{
+				a = aN;
+				r = rN;
+				g = gN;
+				b = bN;
+			}
+			
+			public unsafe colour(byte* s) : this()
+			{
+				a = *(s + 0);
+				r = *(s + 1);
+				g = *(s + 2);
+				b = *(s + 3);
+			}
+			
+			public unsafe colour(int* s) : this()
+			{
+				argb = *s;
+			}
+			
+			public colour clone(int aN, int rN, int gN, int bN)
+			{
+				if (aN < 0)
+					aN = a;
+				if (rN < 0)
+					rN = r;
+				if (gN < 0)
+					gN = g;
+				if (bN < 0)
+					bN = b;
+				
+				return new colour(util.clampToByte(aN),
+				                  util.clampToByte(rN),
+				                  util.clampToByte(gN),
+				                  util.clampToByte(bN));
+			}
+			
+			public colour mul(float ac, float rc, float gc, float bc)
+			{
+				return new colour(util.clampToByte(ac * (float)a),
+				                  util.clampToByte(rc * (float)r),
+				                  util.clampToByte(gc * (float)g),
+				                  util.clampToByte(bc * (float)b));
+			}
+			
+			public colour add(int ac, int rc, int gc, int bc)
+			{
+				return new colour(util.clampToByte(ac + (int)a),
+				                  util.clampToByte(rc + (int)r),
+				                  util.clampToByte(gc + (int)g),
+				                  util.clampToByte(bc + (int)b));
+			}
+		}
+		
+		public class util
+		{
+			public delegate int getFunc(int num, int top);
+			
+			public static int wrap(int num, int top)
+			{
+				return num % top;
+			}
+			
+			public static int mirror(int num, int top)
+			{
+				while (true)
+				{
+					if (num < 0)
+						num = -1 - num;
+					else if (num > top)
+						num = (top * 2 + 1) - num;
+					else
+						return num;
+				}
+			}
+			
+			public static int clamp(int num, int top)
+			{
+				if (num < 0)
+					return 0;
+				else if (num > top)
+					return top;
+				else
+					return num;
+			}
+			
+			public static byte clampToByte(int num)
+			{
+				if (num < 0)
+					return 0;
+				else if (num > 255)
+					return 255;
+				else
+					return (byte)num;
+			}
+			
+			public static byte clampToByte(float num)
+			{
+				return clampToByte((int)num);
+			}
+		}
+		
+		public unsafe abstract class surface
+		{
+			int widthL;
+			int heightL;
+			
+			public int width
+			{
+				get { return widthL; }
+				protected set { widthL = value; }
+			}
+			
+			public int height
+			{
+				get { return heightL; }
+				protected set { heightL = value; }
+			}
+			
+			public abstract void close();
+			public abstract colour getCol(int x, int y);
+			public abstract void setCol(int x, int y, colour c);
+			
+			public colour getCol(int x, int y, util.getFunc gf)
+			{
+				return getCol(gf.Invoke(x, width - 1), gf.Invoke(y, height - 1));
+			}
+			
+			public void setCol(int x, int y, colour c, util.getFunc gf)
+			{
+				setCol(gf.Invoke(x, width - 1), gf.Invoke(y, height - 1), c);
+			}
+			
+			public colour getCol_wrap(int x, int y)
+			{
+				return getCol(x, y, util.wrap);
+			}
+			
+			public void setCol_wrap(int x, int y, colour c)
+			{
+				setCol(x, y, c, util.wrap);
+			}
+			
+			public colour getCol_mirror(int x, int y)
+			{
+				return getCol(x, y, util.mirror);
+			}
+			
+			public void setCol_mirror(int x, int y, colour c)
+			{
+				setCol(x, y, c, util.mirror);
+			}
+			
+			public colour getCol_clamp(int x, int y)
+			{
+				return getCol(x, y, util.clamp);
+			}
+			
+			public void setCol_clamp(int x, int y, colour c)
+			{
+				setCol(x, y, c, util.clamp);
+			}
+			
+			public virtual unsafe void save(string fileName)
+			{
+				Bitmap bmp = new Bitmap(width, height);
+				sdi.BitmapData bmpDat = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), sdi.ImageLockMode.WriteOnly, sdi.PixelFormat.Format32bppArgb);
+				
+				int* s = (int*)bmpDat.Scan0;
+				
+				for (int y = 0; y < height; y++)
+				{
+					for (int x = 0; x < width; x++)
+					{
+						*s = getCol(x, y).argb;
+						s++;
+					}
+				}
+				
+				bmp.UnlockBits(bmpDat);
+				bmp.Save(fileName, sdi.ImageFormat.Png);
+				bmp.Dispose();
+			}
+		}
+		
+		public unsafe class bmpSurface : surface
+		{
+			private Bitmap bmp;
+			private sdi.BitmapData bmpDat;
+			
+			public bmpSurface(string fileName)
+			{
+				Bitmap bmpN = (Bitmap)Bitmap.FromFile(fileName);
+				init(bmpN);
+				bmpN.Dispose();
+			}
+			
+			public bmpSurface(Bitmap bmpN)
+			{
+				init(bmpN);
+			}
+			
+			void init(Bitmap bmpN)
+			{
+				bmp = ((Bitmap)bmpN.Clone()).Clone(new Rectangle(0, 0, bmpN.Width, bmpN.Height), sdi.PixelFormat.Format32bppArgb);
+				
+				bmpDat = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), sdi.ImageLockMode.ReadWrite, sdi.PixelFormat.Format32bppArgb);
+				width = bmpDat.Width;
+				height = bmpDat.Height;
+			}
+			
+			public override void close()
+			{
+				bmp.UnlockBits(bmpDat);
+				bmp.Dispose();
+			}
+			
+			public override colour getCol(int x, int y)
+			{
+				int* s = (int*)bmpDat.Scan0;
+				
+				return new colour(s + y * width + x);
+			}
+			
+			public override void setCol(int x, int y, colour c)
+			{
+				int* s = (int*)bmpDat.Scan0;
+				
+				*(s + y * width + x) = c.argb;
+			}
+		}
+		
+		public class shader
+		{
+			string codeString;
+			sing.Hosting.ScriptEngine pyEngine;
+			sing.Hosting.ScriptScope pyScope;
+			sing.Hosting.ScriptSource pySource;
+			sing.Hosting.CompiledCode pyCode;
+			
+			public shader(string codeN)
+			{
+				codeString = codeN;
+				init();
+			}
+			
+			void init()
+			{
+				try
+				{
+					pyEngine = py.Hosting.Python.CreateEngine();
+					pyScope = pyEngine.CreateScope();
+					pySource = pyEngine.CreateScriptSourceFromString(codeString, sing.SourceCodeKind.Statements);
+					pyCode = pySource.Compile();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message, "Invalid Python");
+				}
+			}
+			
+			public void process(surface target, surface[] surfs)
+			{
+				
+				pyScope.SetVariable("target", target);
+				pyScope.SetVariable("surfs", surfs);
+				
+				try
+				{
+					pyCode.Execute(pyScope);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message);
+				}
+			}
+		}
+		
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void ProcessToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			if (inImgLoc == null)
+			{
+				MessageBox.Show("Please provide an input image");
+				return;
+			}
+			if (outImgLoc == null)
+			{
+				outImgLoc = "default.png";
+			}
+			
+			Bitmap tmap = new Bitmap(inImg.Image.Width, inImg.Image.Height);
+			
+			surface insurf = new bmpSurface((Bitmap)inImg.Image);
+			surface target = new bmpSurface(tmap);
+			
+			shader shade = new shader(codeF.Text);
+			shade.process(target, new surface[] { insurf });
+			
+			target.save(outImgLoc);
+			
+			insurf.close();
+			target.close();
+			tmap.Dispose();
+			
+			updateImgs();
+		}
+		
+		string inImgLoc = null;
+		void InImgClick(object sender, EventArgs e)
+		{
+			DialogResult res = openFileDialog1.ShowDialog();
+			
+			if (res == DialogResult.OK && System.IO.File.Exists(openFileDialog1.FileName))
+				inImgLoc = openFileDialog1.FileName;
+			
+			updateImgs();
+		}
+		
+		string outImgLoc = null;
+		void OutImgClick(object sender, EventArgs e)
+		{
+			DialogResult res = saveFileDialog1.ShowDialog();
+			
+			if (res == DialogResult.OK)
+				outImgLoc = saveFileDialog1.FileName;
+			
+			updateImgs();
+		}
+		
+		void updateImgs()
+		{
+			if (System.IO.File.Exists(inImgLoc))
+				inImg.ImageLocation = inImgLoc;
+			if (System.IO.File.Exists(outImgLoc))
+				outImg.ImageLocation = outImgLoc;
+		}
+	}
+}
